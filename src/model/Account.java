95edb0247613ab9fe2d43d2f21be46eba24b7b67
Nermin <nrmeen.ra.123@gmail.com>
2022-11-29 14:39:47 +0200

    implementing Account and Player classes

--- src/model/Account.java
+++ src/model/Account.java
@@ -1,4 +1,27 @@
 package src.model;
 
 public class Account {
+    private int id;
+    private String nickname;
+
+    public Account(int id, String nickname) {
+        this.id = id;
+        this.nickname = nickname;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
 }
--- src/model/Player.java
+++ src/model/Player.java
@@ -1,4 +1,36 @@
 package src.model;
 
+
 public class Player extends Account{
+
+    private int totalGamesPlayed;
+    private int highestScore;
+
+    public Player(int id, String nickname) {
+        super(id, nickname);
+    }
+
+    public Player(int id, String nickname, int totalGamesPlayed, int highestScore) {
+        super(id, nickname);
+        this.totalGamesPlayed = totalGamesPlayed;
+        this.highestScore = highestScore;
+    }
+
+    public int getTotalGamesPlayed() {
+        return totalGamesPlayed;
+    }
+
+    public void setTotalGamesPlayed(int totalGamesPlayed) {
+        this.totalGamesPlayed = totalGamesPlayed;
+    }
+
+    public int getHighestScore() {
+        return highestScore;
+    }
+
+    public void setHighestScore(int highestScore) {
+        this.highestScore = highestScore;
+    }
+
+
 }
