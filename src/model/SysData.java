c07bf81c784dae71d7db9dee2c0477a0a5c57087
Nermin <nrmeen.ra.123@gmail.com>
2022-11-29 12:17:20 +0200

    added SysData

--- src/model/SysData.java
+++ src/model/SysData.java
@@ -1,5 +1,26 @@
 package sample.src.model;
 
+import java.util.ArrayList;
+import sample.src.model.Question;
+import sample.src.model.Game;
+
 public class SysData {
+
+    private static SysData instance = null;
+    private static final int HIGHSCORES_AMOUNT = 10;
+    private ArrayList<Question> questions = new ArrayList<Question>();
+    private ArrayList<Game> scoreboard = new ArrayList<Game>();
+
+    public static SysData getInstance() {
+        if (instance == null)
+        {
+            instance = new SysData();
+        }
+        return instance;
+    }
+
+
+
+
 
 }
