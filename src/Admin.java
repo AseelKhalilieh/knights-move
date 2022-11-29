f5d3460dad77ed854d5f93bf0e6102414d4bf4e6
Nermin <nrmeen.ra.123@gmail.com>
2022-11-29 17:27:14 +0200

    Add Admin class

---
+++ src/Admin.java
@@ -0,0 +1,20 @@
+package src;
+
+import src.model.Account;
+
+public class Admin extends Account {
+
+    private String password;
+
+    public Admin(int id, String nickname) {
+        super(id, nickname);
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
